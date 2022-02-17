c4a98bd99b8d411220a4de1618a920c587274991
Charlie Dev <83170854+CharlieDev9@users.noreply.github.com>
2022-02-17 05:22:48 +0000

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -9,5 +9,7 @@ nano /etc/hosts
 
 pacman -S grub intel-ucode efibootmgr
 
+#mkdir /boot/efi
+#mount /dev/sda2 /boot/efi
 #grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --no-nvram --removable
 #grub-mkconfig -o /boot/grub/grub.cfg
